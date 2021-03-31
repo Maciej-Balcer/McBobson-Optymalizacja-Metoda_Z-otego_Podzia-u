38b79948d06bf376dcf9a0bafa4f4f358eaf9acd
McBobson <maciej.balcer.98@gmail.com>
2021-03-31 16:06:11 +0200

    Przesłanie plików

---
+++ App.py
@@ -0,0 +1,168 @@
+from PyQt5.QtCore import QDateTime, Qt, QTimer
+from PyQt5.QtWidgets import (QApplication, QCheckBox, QComboBox, QDateTimeEdit,
+        QDial, QDialog, QGridLayout, QGroupBox, QHBoxLayout, QLabel, QLineEdit,
+        QProgressBar, QPushButton, QRadioButton, QScrollBar, QSizePolicy,
+        QSlider, QSpinBox, QStyleFactory, QTableWidget, QTabWidget, QTextEdit,
+        QVBoxLayout, QWidget)
+from PyQt5.QtWidgets import QPushButton, QLineEdit, QApplication, QFormLayout, QWidget, QTextEdit, QSpinBox
+import os
+import prog1 as P
+
+
+def info():
+    instr='Instrukcja wprowadzania danych:\n'
+    instr=instr+'Wzór - zmienne należy wpisać jako: x1,x2,...x5\n'
+    instr=instr+'operator potęgowania - ** \n'
+    instr=instr+'funkcja pierwiastkowania kwadratowego - sqrt()\n'
+    instr=instr+'funkcja logarytmu - log(liczba,baza)\n'
+    instr=instr+'funkcje trygonometryczne - sin(),cos(),tan()\n'
+    instr=instr+'********************\n'
+    instr=instr+'Akceptowalny format danych:\n'
+    instr=instr+'punk startowy: x1,x2\n'
+    instr=instr+'kierunek: a1,a2\n'
+    instr=instr+'długość przedziału: liczba całkowita dodatnia\n'
+    return instr
+
+class Window(QWidget):
+#komentarz w app
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Metoda złotego podziału')
+        width = 1000
+        height = 200
+
+
+        self.setMinimumSize(width, height)
+
+        #Tworzenie obiektów
+        self.Wzor = QLineEdit()
+        self.Punkt_Startowy = QLineEdit()
+        self.Kierunek = QLineEdit()
+        self.Dlugosc_Przedzialu = QLineEdit()
+        self.Epsilon = QLineEdit()
+        self.Ilosc_Iteracji = QLineEdit()
+
+        self.Oblicz = QPushButton("Oblicz")
+        self.Wykres = QPushButton("Wykres")
+
+        self.Funkcja_1 = QPushButton("(x1-2)**2 + (x2-2)**2")
+        self.Funkcja_2 = QPushButton("exp(x1-x2)*x2-2*x1")
+        self.Funkcja_3 = QPushButton("sin(x1*x2)-cos(x1)")
+        self.Funkcja_4 = QPushButton("log(x2*x1)")
+        self.Wyjscie = QTextEdit()
+
+        self.create_Dane_Wejsciowe()
+        self.create_Kryteria_Stopu()
+        self.create_Przykladowe_Funkcje()
+        self.create_Pole_terminalu()
+
+        mainLayout = QGridLayout()
+        mainLayout.addWidget(self.Dane_Wejsciowe, 1, 0)
+        mainLayout.addWidget(self.Kryteria_Stopu, 1, 1)
+        mainLayout.addWidget(self.Przykladowe_Funkcje, 2, 0, 2, 2)
+        mainLayout.addWidget(self.Pole_terminalu,1,3,4,1)
+        mainLayout.addWidget(self.Oblicz,5,3)
+        mainLayout.addWidget(self.Wykres,6,3)
+        self.setLayout(mainLayout)
+
+        #Guzik klika
+        self.Oblicz.clicked.connect(self.Submit)
+        self.Wykres.clicked.connect(self.Wypisz_Wymaluj)
+        self.Funkcja_1.clicked.connect(self.Przypisz1)
+        self.Funkcja_2.clicked.connect(self.Przypisz2)
+        self.Funkcja_3.clicked.connect(self.Przypisz3)
+        self.Funkcja_4.clicked.connect(self.Przypisz4)
+
+        self.Epsilon.setText("0.0001")
+        self.Ilosc_Iteracji.setText("100")
+
+        self.Punkt_Startowy.setText('0,0')
+        self.Kierunek.setText('1,1')
+        self.Dlugosc_Przedzialu.setText('10')
+        self.Wyjscie.setText(info())
+
+    #Jakieśtam funckje pomocnicze
+    def Wypisz_Wymaluj(self):
+        if P.N!=2:
+            self.Wyjscie.setText('Wymiar uniemożliwia wizualizację (wymiar musi być równy 2)')
+            return
+        else:
+            P.wykres()
+
+    def Submit(self):
+        P.Clear()
+        P.Wczytaj_Funkcje(self.Wzor.text())
+        P.Wczytaj_Punk_Poczatkowy(self.Punkt_Startowy.text())
+        P.Podaj_Kierunek(self.Kierunek.text())
+
+        if (len(P.x_start)!=len(P.kier))or(len(P.x_start)!=P.N)or(len(P.kier)!=P.N):
+            self.Wyjscie.setText('Niezgodność wymiarów (wzór, punkt startowy, wektor kierunkowy)!')
+            return
+
+        P.Dlugosc_Przedzialu(int(self.Dlugosc_Przedzialu.text()))
+        P.EPS = float(self.Epsilon.text())
+        P.MAX_ITER = int(self.Ilosc_Iteracji.text())
+
+        self.Wyjscie.setText(P.zwroc_wynik())
+
+
+    def create_Dane_Wejsciowe(self):
+        self.Dane_Wejsciowe = QGroupBox("Dane Wejściowe")
+
+        layout = QFormLayout()
+
+        layout.addRow("Wzor funkcji", self.Wzor)
+        layout.addRow("Punkt startowy", self.Punkt_Startowy)
+        layout.addRow("Kierunek", self.Kierunek)
+        layout.addRow("Dlugość przedziału", self.Dlugosc_Przedzialu)
+
+        self.Dane_Wejsciowe.setLayout(layout)
+
+    def create_Kryteria_Stopu(self):
+        self.Kryteria_Stopu = QGroupBox("Kryteria Stopu")
+
+        layout = QFormLayout()
+
+        layout.addRow("Epsilon", self.Epsilon)
+        layout.addRow("Ilość iteracji", self.Ilosc_Iteracji)
+
+        self.Kryteria_Stopu.setLayout(layout)
+
+    def create_Przykladowe_Funkcje(self):
+        self.Przykladowe_Funkcje = QGroupBox("Przykładowe funkcje")
+        layout = QFormLayout()
+
+        layout.addWidget(self.Funkcja_1)
+        layout.addWidget(self.Funkcja_2)
+        layout.addWidget(self.Funkcja_3)
+        layout.addWidget(self.Funkcja_4)
+
+        self.Przykladowe_Funkcje.setLayout(layout)
+
+    def create_Pole_terminalu(self):
+        self.Pole_terminalu = QGroupBox("Wyjście")
+        layout = QFormLayout()
+
+        layout.addWidget(self.Wyjscie)
+
+        self.Pole_terminalu.setLayout(layout)
+
+    def Przypisz1(self):
+        self.Wzor.setText("(x1-2)**2 + (x2-2)**2")
+
+    def Przypisz2(self):
+        self.Wzor.setText("exp(x1-x2)*x2-2*x1")
+
+    def Przypisz3(self):
+        self.Wzor.setText("sin(x1*x2)-cos(x1)")
+
+    def Przypisz4(self):
+        self.Wzor.setText("log(x2*x1)")
+
+
+
+
+app = QApplication([])
+w = Window()
+w.show()
+app.exec()
---
+++ prog1.py
@@ -0,0 +1,252 @@
+import parser
+import matplotlib.pyplot as plt
+import matplotlib.tri as tri
+import numpy as np
+import math
+from math import exp,e
+from numpy import log,sin,cos,tan,sqrt
+import matplotlib.mlab as ml
+from matplotlib import cm
+
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+from mpl_toolkits.mplot3d import Axes3D
+#komentarz w prog1
+#================= zmienne globalne ===============
+MAX_ITER=100
+EPS=0.0001
+x1=0
+x2=0
+x3=0
+x4=0
+x5=0
+
+wzor = ''
+N = 0
+x_start =[]
+x_stop = []
+kier = []
+dl_przedzialu = 0
+dl_kier=0
+iter = 0
+out=[]
+SZEF=[]
+ACC=7
+
+def Clear():
+    global wzor,N,x_start,x_stop,kier,dl_przedzialu,dl_kier,out,SZEF
+
+    wzor = ''
+    N = 0
+    x_start =[]
+    x_stop = []
+    kier = []
+    dl_przedzialu = 0
+    iter = 0
+    dl_kier=0
+    out=[]
+    SZEF=[]
+
+def Wczytaj_Funkcje(F):
+    global wzor,N
+    wzor = F
+    N=Roz_wzor(wzor)
+
+def Wczytaj_Punk_Poczatkowy(P):
+    global N,string,x_start
+    string=P.split(',')
+    for i in string:
+        x_start.append(int(i))
+
+def Podaj_Kierunek(K):
+    global kier,dl_kier
+    string=K.split(',')
+    for i in string:
+        kier.append(int(i))
+
+    pom=0
+    for i in range(N):
+        pom=pom+kier[i]**2
+    dl_kier=math.sqrt(pom)
+
+def Dlugosc_Przedzialu(DP):
+    global dl_przedzialu,x_stop
+    dl_przedzialu = DP
+
+    for i in range(N):
+        x_stop.append(x_start[i]+kier[i]*dl_przedzialu)
+
+
+def Roz_wzor(string):
+    data=[]
+    for i in range(len(string)):
+        if string[i]=='x':
+            if string[i+1]!='p':
+                data.append(string[i+1])
+
+    odp=[]
+    for i in data:
+        if i not in odp:
+            odp.append(i)
+
+    return len(odp)
+
+
+def f(kier,x0,alfa):
+    global x1,x2,x3,x4,x5,out
+    out=dekompost(kier,x0,alfa)
+
+    ustaw_x(out)
+    exe=eval(wzor)
+    return exe
+
+# funkcja zmieniająca problem n wymiarowy w jeden wymiar
+def dekompost(kier,x0,alfa):
+    odp=[]
+    for i in range(len(kier)):
+        odp.append(x0[i]+kier[i]*alfa)
+    return odp
+
+# ustawia do zmiennych globalnych wartosci x z dostępnego wektora
+def ustaw_x(x):
+    global x1,x2,x3,x4,x5,x6
+    try:
+        x1=x[0]
+        x2=x[1]
+        x3=x[2]
+        x4=x[3]
+        x5=x[4]
+        x6=x[5]
+    except IndexError:
+        pass
+
+# Algorytm złotego podziału
+def golden_method(a,b):
+    global kier,x_start,SZEF
+
+    x0=x_start
+    k=(math.sqrt(5)-1)/2
+
+    alfa_L=b-k*(b-a)
+    alfa_R=a+k*(b-a)
+    iter=0
+    while((alfa_R-alfa_L)>EPS and MAX_ITER>=iter):
+        iter+=1
+        if f(kier,x0,alfa_L) < f(kier,x0,alfa_R):
+            f(kier,x0,alfa_L)
+            SZEF.append(out)
+            b=alfa_R
+            alfa_R=alfa_L
+            alfa_L=b-k*(b-a)
+        else:
+            f(kier,x0,alfa_R)
+            SZEF.append(out)
+            a=alfa_L
+            alfa_L=alfa_R
+            alfa_R=a+k*(b-a)
+
+    return (a+b)/2 , iter
+
+
+
+def zwroc_wynik():
+    global iter
+    wynik,iter=golden_method(0,dl_przedzialu)
+    odp=[]
+    for i in range(N):
+        odp.append(round(x_start[i]+kier[i]*wynik,ACC))
+
+    pom=''
+    string = ''
+    wek = []
+    for i in range(iter):
+        #wek=[SZEF[i][0],SZEF[i][1]]
+        wek = (SZEF[i])
+        ustaw_x(wek)
+        #pom=pom+'('+str(i+1)+') '+"f("+str(round(SZEF[i][0],ACC))+", "+str(round(SZEF[i][1],ACC))+')= '+str(round(eval(wzor),ACC))+'\n'
+        pom = pom+'('+str(i+1)+') '+"f("+str(np.around(SZEF[i],decimals=ACC))+")= "+str(round(eval(wzor),ACC))+'\n'
+    ustaw_x(odp)
+    pom=pom+"\n#### rozwiązanie optymalne #####\n"
+    #string = "x1*= " + str(round(odp[0],ACC)) + " x2*= "+str(round(odp[1],ACC))+"\n" + "f(x1*,x2*)= "+str(round(eval(wzor),12))
+    for i in range(N):
+        string = string + "x"+str(i+1)+"*= "+str(round(odp[i],ACC))+"\n"
+    string = string + "f("
+    for i in range(N):
+        string = string + "x"+str(i+1)+"*, "
+    string = string[:-2]
+    string = string + ")= "+str(round(eval(wzor),12))
+    return pom + string
+
+#######################################################################################################
+def wykres():
+    #POMYSL skalowanie wzgl znaku kierunku wektora zamiast w 4 strony swiata
+    vector = np.vectorize(np.float)
+    npts = 50000
+    ngridx = 200
+    ngridy = 200
+    wsp_zakresu =  dl_kier if(dl_przedzialu <= dl_kier)  else dl_przedzialu
+
+    dl_wek=sqrt((x_start[0]-x_stop[0])**2+(x_start[1]-x_stop[1])**2)
+
+    x1_pom = np.random.uniform(-dl_wek-abs(x_start[0]-1),dl_wek+abs(x_start[0]+1) , npts)
+    x2_pom = np.random.uniform(-dl_wek-abs(x_start[1]-1),dl_wek+abs(x_start[1]+1), npts)
+    #x1_pom=np.random.uniform(-wsp_zakresu-abs(x_start[0]-1),wsp_zakresu+abs(x_start[0]+1),npts)
+    #x2_pom=np.random.uniform(-wsp_zakresu-abs(x_start[1]-1),wsp_zakresu+abs(x_start[1]+1),npts)
+    z=[]
+    for i in range(len(x1_pom)):
+        x1=x1_pom[i]
+        x2=x2_pom[i]
+        z.append(eval(wzor))
+    x1 = x1_pom
+    x2 = x2_pom
+
+
+    fig,ax1=plt.subplots(1,1)
+
+    # Create grid values first.
+    xi = np.linspace(-dl_wek-abs(x_start[0]-1),dl_wek+abs(x_start[0]+1), ngridx)
+    yi = np.linspace(-dl_wek-abs(x_start[0]-1),dl_wek+abs(x_start[0]+1), ngridy)
+
+    # Linearly interpolate the data (x, y) on a grid defined by (xi, yi).
+
+    triang = tri.Triangulation(x1, x2)
+    interpolator = tri.LinearTriInterpolator(triang, z)
+    Xi, Yi = np.meshgrid(xi, yi)
+    zi = interpolator(Xi, Yi)
+
+    # Note that scipy.interpolate provides means to interpolate data on a grid
+    # as well. The following would be an alternative to the four lines above:
+    #from scipy.interpolate import griddata
+    #zi = griddata((x, y), z, (xi[None, :], yi[:, None]), method='linear')
+
+    ax1.contour(xi, yi, zi, levels=14, linewidths=0.5, colors='k')
+    cntr1 = ax1.contourf(xi, yi, zi, levels=14, cmap="RdBu_r")
+
+
+    # punkt do prostej kierunkowej
+    x_start_values=[x_start[0],x_stop[0]]
+    x_stop_values=[x_start[1],x_stop[1]]
+
+    # kolejne punkty iteracyjne
+    xii=[]
+    yii=[]
+    for i in range(iter):
+        xii.append(SZEF[i][0])
+        yii.append(SZEF[i][1])
+
+    # punkty X,Y minimalne
+    x_min=xii[-1]
+    y_min=yii[-1]
+
+    ax1.plot(x_start_values,x_stop_values) #linia kierunku
+    ax1.plot(x_start[0],x_start[1],'kx',markersize=12,label='punkt startowy') # punkt startowy
+    ax1.plot(xii,yii,'k.') # punkty iteracyjne
+    ax1.plot(x_min,y_min,'rx',markersize=12,label='punkt końcowy') #punkt minimalny
+    ax1.legend()
+
+    fig.colorbar(cntr1, ax=ax1)
+
+    #ax1.set(xlim=(dl_kier*(x_start[0]-dl_przedzialu-1), dl_kier*(x_start[0]+dl_przedzialu+1)), ylim=(dl_kier*(x_start[1]-dl_przedzialu-1), dl_kier*(x_start[1]+dl_przedzialu+1))) #to ustawia szerokosc plota
+    ax1.set(xlim=(-dl_wek-abs(x_start[0]-1),dl_wek+abs(x_start[0]+1)), ylim=(-dl_wek-abs(x_start[0]-1),dl_wek+abs(x_start[0]+1))) #to ustawia szerokosc plota
+
+    plt.subplots_adjust(hspace=0.5)
+    plt.show()
